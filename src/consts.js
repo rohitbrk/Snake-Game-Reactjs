9d7b756370467879cb7ac7661b6fd52991263dfa
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2023-10-12 08:27:27 +0530

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -7,10 +7,10 @@ const APPLE_START = [8, 3];
 const SCALE = 40;
 const SPEED = 200;
 const DIRECTIONS = {
-  38: [0, -1], // up
-  40: [0, 1], // down
-  37: [-1, 0], // left
-  39: [1, 0], // right
+  38: [0, -1], 
+  40: [0, 1], 
+  37: [-1, 0], 
+  39: [1, 0], 
 };
 
 export { CANVAS_SIZE, SNAKE_START, APPLE_START, SCALE, SPEED, DIRECTIONS };
